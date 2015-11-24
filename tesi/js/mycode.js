06286498068a27ae7e7a628454d041621d989e37
Matteo Palumbo <matteo.p85@gmail.com>
2015-11-24 18:39:14 +0100

    Added uml diagram prototype

---
+++ tesi/js/mycode.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  var select = $('select');
+  select.material_select();
+
+  // $('#newElement select').change(function(){
+  //     if($(this).val() == 'newClass'){
+  //       $('#newClassTab').show().siblings("div").hide();
+  //     }else if($(this).val() == 'newAbstractClass'){
+  //       $('#newAbstractClassTab').show().siblings("div").hide();
+  //     }else if($(this).val() == 'newInterface'){
+  //       $('#newInterfaceTab').show().siblings("div").hide();
+  //     }else if($(this).val() == 'newRelation'){
+  //       $('#newRelationTab').show().siblings("div").hide();
+  //     }
+  //   });
+
+});
+
+
